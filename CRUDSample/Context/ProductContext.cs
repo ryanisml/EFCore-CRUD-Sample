d71c9eb29b08b1dd91403bbac4d725248da73798
ryanisml <ryanisml@gmail.com>
2023-08-30 11:35:36 +0800

    Add project files.

---
+++ CRUDSample/Context/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿namespace waterlevel_netcore.Models
+{
+	public class ProductContext : DbContext
+	{
+        public ProductContext() { }
+
+        public ProductContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<ProductContext> ultrasoniks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) {
+                
+            }
+        }
+    }
+}
---
+++ CRUDSample/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace waterlevel_netcore.Models
+{
+    [Table("tb_product")]
+    public partial class ProductModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string nama_barang { get; set; }
+        public double harga { get; set; }
+        public string stok { get; set; }
+    }
+}
+
